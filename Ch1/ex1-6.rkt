dc9fc5fd5b57eb45bfcdef27fa75d07d0e609b61
leopardshadow <thurtu811@gmail.com>
2020-09-27 20:43:29 +0800

    add 1-6 & sqrt example

---
+++ Ch1/ex1-6.rkt
@@ -0,0 +1,37 @@
+#lang sicp
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(new-if (= 2 3) 0 5)
+(new-if (= 1 1) 0 5)
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 2)
+(sqrt 3)
+
+;; out of memory !!
+;; since new-if is just a normal function, not like "if"
+;; applicative order is applied, every parameter will be evaluated
+;; so the iteration will go on, the program never stops
