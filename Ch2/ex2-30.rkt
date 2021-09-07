d0dc8bb316087178bd21a85f314ad3a209efce48
leopardshadow <thurtu811@gmail.com>
2021-09-07 20:57:42 +0800

    update 2.30

--- Ch2/ex2-30.rkt
+++ Ch2/ex2-30.rkt
@@ -4,8 +4,8 @@
 
 (define (square-tree tree) (cond ((null? tree) nil)
                                  ((not (pair? tree)) (square tree))
-                                 (else (cons( (square-tree (car tree))
-                                              (square-tree (cdr tree))  )))))
+                                 (else (cons (square-tree (car tree))
+                                              (square-tree (cdr tree))  ))))
 (square-tree (cons 1 2))
 
 (square-tree
