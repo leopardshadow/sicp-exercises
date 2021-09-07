d4e70303963bcfb24145d8351c211e02dc19bf62
leopardshadow <thurtu811@gmail.com>
2021-09-07 22:34:01 +0800

    add 2.33

---
+++ Ch2/ex2-33.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+      (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
+(map (lambda (x) (+ x 2)) (list 1 2 3 4 5))
+
+(append (list 1 2) (list 6 7 8))
+
+(length (list 1 2 3 4 5))
